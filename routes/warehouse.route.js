6ad9c3a5e5b2f824e26ae8fe2637c701494cc1ba
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-08-07 00:50:20 +0700

    Create warehouse route

---
+++ routes/warehouse.route.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import Warehouse from '../models/Warehouse.js';
+
+const router = express.Router();
+
+router.route('/create-warehouse').post(async (req, res, next) => {
+    try {
+        const warehouse = await Warehouse.create(req.body);
+        console.log(warehouse);
+        res.json(warehouse);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.route('/').get(async (req, res, next) => {
+    try {
+        const warehouse = await Warehouse.find();
+        console.log(warehouse);
+        res.json(warehouse);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.route('/edit-warehouse/:id').get(async (req, res, next) => {
+    try {
+        const warehouse = await Warehouse.findById(req.params.id);
+        if (!warehouse) {
+            return res.status(404).json({ message: 'Warehouse not found' });
+        }
+        console.log(warehouse);
+        res.json(warehouse);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.route('/update-warehouse/:id').put((req, res, next) => {
+    Warehouse.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, warehouse) => {
+        if (err) return next(err);
+        res.json(warehouse);
+        console.log("Warehouse updated successfully!");
+    });
+});
+
+router.route('/delete-warehouse/:id').delete((req, res, next) => {
+    Warehouse.findByIdAndRemove(req.params.id, (err, warehouse) => {
+        if (err) return next(err);
+        res.status(200).json({ msg: warehouse });
+    });
+});
+
+export default router;
