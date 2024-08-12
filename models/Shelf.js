8c7cc749a62f3e07f6753a22c6dd774ebd6c3533
bossxdev <suphanut_boss@outlook.com>
2024-08-12 14:25:33 +0700

    Refactor shelf schema

--- models/Shelf.js
+++ models/Shelf.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const ShelfSchema = new Schema({
-    ShelfID: { type: String, required: true, max: 100 },
-    ShelfName: { type: String, required: true, max: 100 },
-    WarehouseID: { type: String, required: true, max: 100 }
+    shelfId: { type: String, required: true, max: 100 },
+    shelfName: { type: String, required: true, max: 100 },
+    warehouseID: { type: String, required: true, max: 100 }
 });
 
 const Shelf = mongoose.model('Shelf', ShelfSchema);
