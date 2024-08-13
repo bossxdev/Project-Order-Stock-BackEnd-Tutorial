99ecf1e5712a2bfaa154eafc95ad6fbec1fdac4d
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-08-13 22:06:28 +0700

    Update shelf schema datetime

--- models/Shelf.js
+++ models/Shelf.js
@@ -5,7 +5,10 @@ const Schema = mongoose.Schema;
 const ShelfSchema = new Schema({
     shelfId: { type: String, required: true, max: 100 },
     shelfName: { type: String, required: true, max: 100 },
-    warehouseId: { type: String, required: true, max: 100 }
+    warehouseId: { type: String, required: true, max: 100 },
+    status: { type: String, max: 100 },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
 });
 
 const Shelf = mongoose.model('Shelf', ShelfSchema);
